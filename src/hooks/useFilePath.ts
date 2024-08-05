25b94233b1eaff562316ad91c8041d53b30ee9c6
aegis <125031666+ae9is@users.noreply.github.com>
2024-08-05 02:43:33 -0700

    5 previews (#16)
    * feat: add thumbnail previews for some repositories
    
    * feat: search for and add thumbnail previews to repository cards
    
    * fix: ignore errors when no thumbnail exists for a repository
    
    * perf: skip thumbnail re-fetching

---
+++ src/hooks/useFilePath.ts
@@ -0,0 +1,53 @@
+// Find any files matching a path plus extensions.
+// I.e. '/path/to/example', ['webp', 'png'] -> '/path/to/example.webp'
+
+import { useEffect, useState } from 'react'
+
+export const imageExtensions = [
+  //
+  'webp',
+  'png',
+  'jpg',
+  'svg',
+]
+
+export function useFilePath(basePath: string, possibleExtensions = imageExtensions) {
+  const [path, setPath] = useState<string | null | undefined>()
+
+  useEffect(() => {
+    if (path !== undefined) {
+      // Don't refetch
+      return
+    }
+    const findPath = async (basePath: string, possibleExtensions: string[]) => {
+      const promises = possibleExtensions.map((ext) =>
+        (async () => {
+          try {
+            const searchPath = `${basePath}.${ext}`
+            const resp = await fetch(searchPath)
+            if (resp?.ok) {
+              // Vite will return 200 even on not found requests, need to sanity check content itself
+              const blob = await resp.blob()
+              if (blob?.size > 0 && blob?.type?.includes('image')) {
+                return searchPath
+              }
+            }
+          } catch (err) {
+            //
+          }
+          throw new Error('reject')
+        })().catch()
+      )
+      try {
+        const found = await Promise.any(promises)
+        setPath(found)
+      } catch (aggregateErr) {
+        // console.warn(`No thumbnail found for ${basePath}`)
+        setPath(null)
+      }
+    }
+    findPath(basePath, possibleExtensions)
+  }, [path, basePath, possibleExtensions])
+
+  return path
+}
